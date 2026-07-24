1494c9cfd8564365cddb71636e9da149bf3f086e
Harshil Jain <kotaharshil2906@gmail.com>
2026-07-24 11:17:56 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        if(s.length()!=t.length()) return false;
+        
+        unordered_map<char,int> mp;
+
+        for(int i=0;i<s.length();i++)
+        {
+            mp[s[i]]++;
+            mp[t[i]]--;
+        }
+
+        for(int i=0;i<mp.size();i++)
+        {
+            if(mp[s[i]]!=0) return false;
+        }
+
+            return true;
+
+    }
+};
