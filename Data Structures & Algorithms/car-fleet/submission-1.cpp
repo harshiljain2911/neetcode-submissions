7c0bdf27803c0c9c1900ac0d7a5b1fda47471d49
Harshil Jain <kotaharshil2906@gmail.com>
2026-08-19 20:37:53 +0530

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+
+        vector<pair<int, int>> cars;
+
+        for (int i = 0; i < position.size(); i++) {
+            cars.push_back({position[i], speed[i]});
+        }
+
+        // Closest to target -> farthest from target
+        sort(cars.rbegin(), cars.rend());
+
+        stack<double> st;
+
+        for (auto car : cars) {
+            double time = (double)(target - car.first) / car.second;
+
+            if (st.empty() || time > st.top()) {
+                st.push(time);
+            }
+        }
+
+        return st.size();
+    }
+};
